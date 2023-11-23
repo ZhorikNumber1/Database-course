7c3bc087f09870c826bad7cc5b72a7b9f154519c
Pavel <109911382+ZhorikNumber1@users.noreply.github.com>
2023-11-24 02:37:32 +0300

    changes according to pull request comments

--- lab_1_point_7.sql
+++ lab_1_point_7.sql
@@ -1,19 +1,8 @@
 /*
 	7 + 10. Из таблицы Production.Document показать поля DocumentNode, Title и FileExtension. 
-	Все пустые значения поля FileExtension заменить на NULL, а затем все NULL этого же поля заменить на '.txt'. 
+	Все пустые значения поля FileExtension заменить на заменить на '.txt'. 
 	Названия полей оставить без изменений.
 */
 
-SELECT "DocumentNode", "Title", "FileExtension"
-FROM "Production"."Document";
-
-UPDATE "Production"."Document"
-SET "FileExtension" = null
-WHERE "FileExtension" = '';
-
-UPDATE "Production"."Document"
-SET "FileExtension" = '.txt'
-WHERE "FileExtension" IS NULL;
-
-SELECT "DocumentNode", "Title", "FileExtension"
+SELECT "DocumentNode", "Title", COALESCE("FileExtension", '.txt') AS "FileExtension"
 FROM "Production"."Document";
